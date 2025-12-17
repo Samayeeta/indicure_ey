7b897ddcb0194390436d8525d05447b1e4a3a58f
Samayeeta <samayeetamaitra202@gmail.com>
2025-12-17 12:50:42 +0530

    Added frontend origin in CORS

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://YOUR-FRONTEND-NAME.onrender.com",
+        "https://indicure-ey-1.onrender.com",
     ],
     allow_credentials=False,
     allow_methods=["*"],
